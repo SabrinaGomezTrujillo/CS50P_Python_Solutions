677c2a98b8f20cca4d446fb7637c9ac3fcf4bf25
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-01-04 13:29:54 +0000

    Sun, Jan 4, 2026, 2:29 PM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,9 +1,7 @@
-text=input("What is the answer to the Great Question of Life, the Universe and Everything? ")
-if text == "42":
+answer=input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+
+if answer == "42" or answer.lower().replace(" ","") == "fortytwo" or answer.lower() == "forty-two":
     print("Yes")
-elif text.lower() == "forty-two":
-    print("Yes")
-elif text.lower() == "forty two":
-    print("Yes")
+
 else:
     print("No")
