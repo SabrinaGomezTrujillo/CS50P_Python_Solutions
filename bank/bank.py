e9a0a8f3ffb5c4dbe1a9169c3a99133acb7206eb
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-01-04 19:58:02 +0000

    Sun, Jan 4, 2026, 8:58 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,6 +1,5 @@
-greeting=input("Gretting: ")
-def main(greeting):
-    if greeting.strip().lower().startswith()=="hello": print("$0")
-    elif greeting.strip()[0].lower()=="h": print("$20")
-    else: print("$100")
-main()
+greeting=input("Greeting:")
+
+if greeting.strip().lower().startswith("hello"): print("$0")
+elif greeting.strip()[0].lower()=="h": print("$20")
+else: print("$100")
