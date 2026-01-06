5d5479b0f73bbe784dea106598d8370aa01686b8
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-01-06 18:51:14 +0000

    Tue, Jan 6, 2026, 7:51 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,18 +1,23 @@
 def main():
-    time = input("What time is it? (HH:MM): ")
 
-    if 7.0 <= convert(time) <= 8.0:
+    time=input("What time is it?: ")
+
+    time_converted = convert(time)
+
+    if 7.0 <= time_converted <= 8.0:
         print("It's breakfast time!")
-    elif 12 <= convert(time) <= 13:
+    elif 12 <= time_converted <= 13:
         print("It's lunch time!")
-    elif 18 <= convert(time) <= 19:
+    elif 18 <= time_converted <= 19:
         print("It's dinner time!")
 
 
+
 def convert(time):
-    hours, minutes = map(int, time.split(":"))
+    hours, minutes = time.split(":")
+    hours = int(hours)
+    minutes = int(minutes)
     return round(float(hours + minutes/60), 2)
 
 
-if __name__ == "__main__":
-    main()
+main()
