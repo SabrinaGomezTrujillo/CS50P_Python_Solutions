28efa43674e14b378b0942165c1682501f232bda
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-01-11 21:41:24 +0000

    Sun, Jan 11, 2026, 10:41 PM +01:00

---
+++ plates/plates.py
@@ -0,0 +1,20 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+def is_valid(s):
+    if len(s)<2 or len(s)>6:
+        return False
+    if not s[0:2].isalpha():
+        return False
+    exclusiones=[".,;:!?\"'()-[]{}...¡¿"]
+    if s in exclusiones:
+        return False
+    for i in range(len(s)):
+        if s[i].isdigit() and s[i]=="0" and s[i-1].isalpha:
+            return False
+    else:
+         return True
+main()  
