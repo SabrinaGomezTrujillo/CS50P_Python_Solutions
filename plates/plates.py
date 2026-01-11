bc14bcf23a05eedd215a62fe51cef0e98f4f65b2
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-01-11 21:58:54 +0000

    Sun, Jan 11, 2026, 10:58 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,7 +7,7 @@ def main():
 def is_valid(s):
     if len(s)<2 or len(s)>6:
         return False
-    if not s[0:2].isalpha():
+    if not s[0].isalpha() and not s[1].isalpha():
         return False
     exclusiones = ".,;:!?\"'()-[]{}¡¿ "
     if s in exclusiones:
