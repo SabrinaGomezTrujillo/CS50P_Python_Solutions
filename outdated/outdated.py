51c0a6a9f7918701375d0ccb799481b2e23d5c72
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-01-15 15:10:39 +0000

    Thu, Jan 15, 2026, 4:10 PM +01:00

---
+++ outdated/outdated.py
@@ -0,0 +1,9 @@
+def main():
+    month=["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    date=input("Date: ")
+
+    #DD<32
+    #PRINT "YYYY-MM-DD"
+    #Value/KeyError - pass
+
+main()
