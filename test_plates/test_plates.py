4a5235b5ddf23d905f6dedb7932d616852d4c3c4
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-01-23 22:17:40 +0000

    Fri, Jan 23, 2026, 11:17 PM +01:00

---
+++ test_plates/test_plates.py
@@ -0,0 +1,29 @@
+from plates import is_valid
+
+
+def test_lenght_min_2():
+    assert is_valid("A") == False
+
+
+def test_lenght_max_6():
+    assert is_valid("AAAA17878") == False
+
+
+def test_first_2_alpha():
+    assert is_valid("AA24") == True
+
+
+def test_no_symbols():
+    assert is_valid("@A#21$") == False
+
+
+def test_no_0_first():
+    assert is_valid("AA0214") == False
+
+
+def test_numbers_after_1st_number():
+    assert is_valid("AA52X4") == False
+
+
+def test_default():
+    assert is_valid("CDE35") == True
