c094ecafd0745870fa6406d96a01767d1836f43f
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-02-02 00:11:09 +0000

    Mon, Feb 2, 2026, 1:11 AM +01:00

---
+++ response/response.py
@@ -0,0 +1,21 @@
+from validator_collection import validators, checkers, errors
+import re
+
+
+def main():
+    s = input("What's your email address?: ")
+    print(validation(s))
+
+
+def validation(s):
+    try:
+        email = validators.email(s)
+        print(email)
+        if checkers.is_email(email):
+            return "Valid"
+    except errors.InvalidEmailError:
+        return "Invalid"
+
+
+if __name__ == "__main__":
+    main()
