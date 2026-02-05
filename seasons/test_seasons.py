e0c469efc3fb3210e27a5954d01c3b8e285af224
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-02-05 17:28:53 +0000

    Thu, Feb 5, 2026, 6:28 PM +01:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,11 +1,11 @@
 
 import pytest
 from datetime import date, datetime, time
-from seasons import validate_date, __str__
+from seasons import conv_text, validate_date
 
 def test_invalid_date():
     with pytest.raises(SystemExit):
         validate_date("Invalid date")
 
-def test_valid_date():
-    assert validate_date(dob=datetime.strptime("2000-01-01", "%Y-%m-%d").date()) == 13726080.0
+def test_conv_text ():
+    assert conv_text("10") == "Ten minutes"
