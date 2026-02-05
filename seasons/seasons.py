af34089730095d425130ce1f0adf1282b326bc29
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-02-05 17:23:51 +0000

    Thu, Feb 5, 2026, 6:23 PM +01:00

---
+++ seasons/seasons.py
@@ -0,0 +1,38 @@
+from datetime import date, datetime, time
+import sys
+import num2words
+
+
+def main():
+
+    print(
+        f"{conv_text(validate_date(input("Date of birth: ")))}"
+    )
+
+
+def validate_date(dob):
+    try:
+        dob = datetime.strptime(dob, "%Y-%m-%d").date()
+    except ValueError:
+        print("Invalid date")
+        sys.exit(1)
+    # replace_words = calc_minutos(dob)
+
+    fecha1 = datetime.combine(dob, time.min)
+    fecha2 = datetime.combine(
+        date.today(), time.min
+    )  # imprime la fecha de hoy a las 00:00:00
+    delta = fecha2 - fecha1
+    total_minutos = delta.total_seconds() // 60  # segundos en un minuto = 60
+    return total_minutos
+
+
+
+def conv_text(total_minutos):
+    num2words.num2words(total_minutos, lang="en")
+    replace_words = num2words.num2words(total_minutos, lang="en").replace(" and ", " ")
+    return f"{replace_words.capitalize()} minutes"
+
+
+if __name__ == "__main__":
+    main()
