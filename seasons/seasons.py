a5edb6e79f4345e498b776cfad82f4a8974f5e10
Sabri667 <120385369+Sabri667@users.noreply.github.com>
2026-02-05 17:57:19 +0000

    Thu, Feb 5, 2026, 6:57 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,34 +1,36 @@
-from datetime import date, datetime, time
+rom datetime import date, datetime, time
 import sys
-import num2words
+import inflect
 
 
 def main():
-
     dob=input("Date of birth: ")
     total_minutos = validate_date(dob)
     print(f"{conv_text(total_minutos)}")
 
 
 
-    def validate_date(dob):
-        try:
-            dob = datetime.strptime(dob, "%Y-%m-%d").date()
-            fecha1 = datetime.combine(dob, time.min)
-            fecha2 = datetime.combine(date.today(), time.min)  # imprime la fecha de hoy a las 00:00:00
-            delta = fecha2 - fecha1
-            total_minutos = delta.total_seconds() / 60  # segundos en un minuto = 60
-            return total_minutos
-        except ValueError:
-            print("Invalid date")
-            sys.exit(1)
-        # replace_words = calc_minutos(dob)
+def validate_date(dob):
+    try:
+        dob = datetime.strptime(dob, "%Y-%m-%d").date()
+    except ValueError:
+        print("Invalid date")
+        sys.exit(1)
+    # replace_words = calc_minutos(dob)
+
+    fecha1 = datetime.combine(dob, time.min)
+    fecha2 = datetime.combine(
+        date.today(), time.min
+    )  # imprime la fecha de hoy a las 00:00:00
+    delta = fecha2 - fecha1
+    total_minutos = delta.total_seconds() // 60  # segundos en un minuto = 60
+    return total_minutos
 
 
-    def conv_text(total_minutos):
-        num2words.num2words(total_minutos, lang="en")
-        replace_words = num2words.num2words(total_minutos, lang="en").replace(" and ", " ")
-        return f"{replace_words.capitalize()} minutes"
+
+def conv_text(total_minutos):
+    p = inflect.engine()
+    return p.number_to_words(total_minutos, andword="").capitalize() + " minutes"
 
 
 if __name__ == "__main__":
